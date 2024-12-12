19bb4a42ee467c9e8569c0bbcb8c688a4b4807bc
MacThings <info@sl-soft.de>
2024-12-12 09:10:14 +0100

    Fixes
    Fixes

--- compile_apple.sh
+++ compile_apple.sh
@@ -5,6 +5,7 @@
 # brew install sdl2 sdl2_mixer sdl2_image sdl2_ttf git qt make 
 # arch -x86_64 /usr/local/bin/brew install sdl2 sdl2_mixer sdl2_image sdl2_ttf git qt make 
 
-qmake 
+qmake
+export CPATH=/opt/homebrew/include:$CPATH
+export LIBRARY_PATH=/opt/homebrew/lib:$LIBRARY_PATH
 make
-
